08dba42e92305cc57883f368017477ceb5c12f1b
Vlad Frangu <kingdgrizzle@gmail.com>
2020-05-11 15:18:08 +0300

    chore: Fix test..again

--- test/Util.ts
+++ test/Util.ts
@@ -1,5 +1,5 @@
 import ava from 'ava';
-import { Routes } from '../dist/src';
+import { Routes } from '../dist';
 
 ava('sample test', (test): void => {
 	test.is(Routes.oauthApplication(), '/oauth2/applications/@me');
