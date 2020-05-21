84bb7787fef83eee31d3aefba7c5f9871fd97127
bdistin <bdistin@gmail.com>
2020-05-21 16:12:43 -0500

    switch to @ava/typescript

--- test/Util.ts
+++ test/Util.ts
@@ -1,5 +1,5 @@
 import ava from 'ava';
-import { Routes } from '../dist';
+import { Routes } from '../src';
 
 ava('sample test', (test): void => {
 	test.is(Routes.oauthApplication(), '/oauth2/applications/@me');
